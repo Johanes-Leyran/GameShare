fb6636dda29f5eadccefd19c3b7c31e1acdc70ee
Johanes-Leyran <102525247+Johanes-Leyran@users.noreply.github.com>
2024-09-30 18:45:36 +0800

    added custom user GameShareUser

--- gameshare/accounts/models.py
+++ gameshare/accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class GameShareUser(AbstractUser):
+    email = models.EmailField(
+        unique=True, 
+        help_text=_("The email for your account")
+    )
+    username = models.CharField(
+        max_length=120,
+        unique=True,
+        help_text=_("The username for your account")
+    )
+    show_adult_content = models.BooleanField(
+        default=False,
+        help_text=_("Show adult contents to the user")   
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'username'
+    ]
