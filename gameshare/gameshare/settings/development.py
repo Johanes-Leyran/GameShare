1a706d1476945ea4e35100b9706eaaafa10e981a
Johanes-Leyran <102525247+Johanes-Leyran@users.noreply.github.com>
2024-11-22 20:17:09 +0800

    add accounts to installed apps add share to installed apps

--- gameshare/gameshare/settings/development.py
+++ gameshare/gameshare/settings/development.py
@@ -23,7 +23,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'accounts.apps.AccountsConfig',
+    'accounts',
+    'share',    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
