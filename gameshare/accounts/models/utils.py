e8229618332ecd42064e1f7b1d6088f9ab60c27f
Johanes-Leyran <102525247+Johanes-Leyran@users.noreply.github.com>
2024-11-22 21:06:08 +0800

    - remove the checking if a instance of associate already exists since it redundant - minor chaanges

--- gameshare/accounts/models/utils.py
+++ gameshare/accounts/models/utils.py
@@ -1,16 +1,16 @@
 from associates import Associate    
 from utils.models import get_object_or_none
+from django.conf import settings
+
+
+user_model = settings.AUTH_USER_MODEL
 
 # returns false if it fails to do its task
 def add_associate(from_user, to_user, relationship_type):
-    obj = get_object_or_none(
-        Associate, 
-        from_user=from_user,
-        to_user=to_user,
-        relationship_type=relationship_type
-    )
+    to_user = get_object_or_none(user_model, pk=to_user.pk)
+    from_user = get_object_or_none(user_model, pk=from_user.pk)
 
-    if not obj:
+    if to_user and from_user:
         return Associate.objects.create(
             from_user=from_user,
             to_user=to_user,
